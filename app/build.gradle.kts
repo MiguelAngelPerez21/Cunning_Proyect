aa06a560d56ecdc81a49e4ad49553ccb69159c10
MiguelAngelPerez21 <CanonigoOnce11@>
2026-02-02 13:47:41 +0100

    New Changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
-}
+
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+
+    }
 
 android {
     namespace = "com.example.cunning_proyect"
@@ -51,4 +55,16 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment:2.7.7")
     implementation("androidx.navigation:navigation-ui:2.7.7")
     implementation("com.google.android.material:material:1.11.0")
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.8.0"))
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
 }
